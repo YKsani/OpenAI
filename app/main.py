4eeaf0efcebec065a1c1da78b660b5178aff6efa
YKsani <yan-kowalski@mail.ru>
2023-01-17 03:35:31 +0500

    version 0.1

---
+++ app/main.py
@@ -0,0 +1,24 @@
+import openai
+import telebot
+import os
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
+bot = telebot.TeleBot(os.getenv('API_TOKEN'))
+openai.api_key = os.getenv('openai.api_key')
+
+@bot.message_handler(func=lambda _: True)
+def handle_message(message):
+  response = openai.Completion.create(
+    model="text-davinci-003",
+    prompt=message.text,
+    temperature=0.7,
+    max_tokens=1024,
+    top_p=1.0,
+    frequency_penalty=0.0,
+    presence_penalty=0.0,
+  )
+  bot.send_message(chat_id=message.from_user.id, text=response['choices'] [0] ['text'])
+
+
+bot.polling()
